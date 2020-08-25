74c5ca72428115fe4fee6bfc5a86d8904c6bef93
JunichiYukawa <yukawa@ohmae.ac.jp>
2020-08-25 17:56:28 +0900

    fix

--- top/urls.py
+++ top/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.IndexView.as_view()),
     path('sync$', views.sync, name='sync'),
-    path('<str:name>$', views.DetailView.as_view(), name="detail"),
+    path('<str:name>', views.DetailView.as_view(), name="detail"),
     path('<str:name>/<str:target>/<int:rev>', views.RevisionView.as_view(), name="revision")
 ]
